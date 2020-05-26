d6e0ea84e86b679129e6aa024533cbbbdf1902a0
Juan Carlos Condori <juanc.com@gmail.com>
2020-05-26 15:16:20 -0400

    Agrupación de rutas y adición de ruta productos

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Contacto from '../views/Contacto.vue'
 import Usuarios from '../views/Usuarios.vue'
 import Login from '../views/Login.vue'
 import Signup from '../views/Signup.vue'
+import Productos from '../views/productos/Index.vue'
 
 Vue.use(VueRouter)
 
@@ -13,38 +14,36 @@ Vue.use(VueRouter)
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
+  }, {
     path: '/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
+  }, {
     path: '/contacto',
     name: 'Contacto',
     component: Contacto
-  },
-  {
+  }, {
     path: '/usuarios/:id',
     name: 'Usuarios',
     component: Usuarios
-  },
-  {
+  }, {
     path: '/login',
     name: 'Login',
     component: Login
-  },
-  {
+  }, {
     path: '/signup',
     name: 'Signup',
     component: Signup
-  },
-  {
+  }, {
     path: '/registrarse',
     redirect: '/signup'
+  }, {
+    path: '/productos',
+    name: 'Productos',
+    component: Productos
   }
 ]
 
---
+++ src/views/productos/Index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div class="cover">
+            <div class="col-md-12">
+                <h3>Productos</h3>
+            </div>
+            <div class="row">
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+    .cover{
+        padding: 20px;
+        margin: 40px 2px;
+        border: 1px solid #D2E0E6;
+    }
+</style>
